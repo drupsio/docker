9f22740c074aa0cd9b294b0e69a80e888a7e537b
Temuri Takalandze <takalandzet@gmail.com>
2021-04-08 17:13:02 +0400

    :sparkles: Implement registry service.

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -14,8 +14,10 @@ source "${DIR}"/../.env
 source "${DIR}"/styles.env
 source "${DIR}"/functions.sh
 
-TOTAL_STEPS=3
+TOTAL_STEPS=4
 CURRENT_STEP=0
+
+CERTS_DIR="${DIR}"/../certs
 
 echo -e "${COLOR_GREEN} Preparing for installation...${COLOR_NONE}\n"
 
@@ -26,6 +28,19 @@ clone_repository "${ENGINE_GIT_REPO}" "${ENGINE_GIT_BRANCH}" "${ENGINE_DIR}"
 CURRENT_STEP=$((CURRENT_STEP + 1))
 echo -e "\n\v${COLOR_BLUE}[${CURRENT_STEP}/${TOTAL_STEPS}] Cloning Application repository...\n\v${COLOR_NONE}"
 clone_repository "${APPLICATION_GIT_REPO}" "${APPLICATION_GIT_BRANCH}" "${APPLICATION_DIR}"
+
+CURRENT_STEP=$((CURRENT_STEP + 1))
+echo -e "\n\v${COLOR_BLUE}[${CURRENT_STEP}/${TOTAL_STEPS}] Creating certificates for Local Docker Registry...\n\v${COLOR_NONE}"
+mkdir -p "${CERTS_DIR}"/registry/{crt,key}
+openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes \
+  -out "${CERTS_DIR}"/registry/crt/ca.crt \
+  -keyout "${CERTS_DIR}"/registry/key/ca.key \
+  -subj '/CN=registry.loc' \
+  -addext "subjectAltName = DNS:registry.loc"
+
+# Make self-signed certificate trusted for Docker.
+sudo mkdir /etc/docker/certs.d/registry.loc -p
+sudo cp "${CERTS_DIR}"/registry/crt/ca.crt /etc/docker/certs.d/registry.loc/
 
 CURRENT_STEP=$((CURRENT_STEP + 1))
 echo -e "\n\v${COLOR_BLUE}[${CURRENT_STEP}/${TOTAL_STEPS}] Restarting containers...\n\v${COLOR_NONE}"
